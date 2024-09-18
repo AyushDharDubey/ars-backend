d717de3c278007198974309019ce6312fcb6d444
Ayush Dhar Dubey <mailaddubey@gmail.com>
2024-09-18 16:36:47 +0530

    add default db credentials

--- ars/settings.py
+++ ars/settings.py
@@ -90,8 +90,8 @@ WSGI_APPLICATION = 'ars.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE'),
-        'NAME': os.environ.get('DB_NAME'),
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('DB_NAME', 'db.sqlite3'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
