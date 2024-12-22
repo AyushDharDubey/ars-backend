16ed5fca687c81a5447be1ef2e63f55edcbfc4d7
Ayush Dhar Dubey <mailaddubey@gmail.com>
2024-12-22 11:16:32 +0530

    serve static

--- ars/urls.py
+++ ars/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('api/', include('assignment.urls')),
     path('reviewer/', include('reviewer.urls')),
     path('reviewee/', include('reviewee.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
